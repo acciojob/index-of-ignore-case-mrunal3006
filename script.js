adc8ea0423af6c8cba1b1760287a8296728fcf0a
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-07 19:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 // Handle edge cases
+  if (str.length === 0 || subStr.length === 0) {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  // Find the index of the first occurrence of subStr in str
+  const index = lowerStr.indexOf(lowerSubStr);
+
+  return index;
 }
 
 // Please do not change the code below
